f4128fbd1b9657fc22bc78783ca99b22ee33e162
dina3538 <dinaaltayeb1234@gmail.com>
2023-11-28 06:06:39 -0800

    a

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for alpha in str:
         temp = alpha
-        if ord(temp) >= and ord(temp) <= 122:
+        if ord(temp) >= 97 and ord(temp) <= 122:
             temp = chr(ord(alpha) - 32)
         print("{}".format(temp), end="")
     print()
