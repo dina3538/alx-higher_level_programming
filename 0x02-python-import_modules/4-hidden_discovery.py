913bc2d7b878f2e6960fdbc4bf3ecd217fb30637
dina3538 <dinaaltayeb1234@gmail.com>
2023-11-30 02:33:02 -0800

    a

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """print hidden dirctory"""
+    import hidden_4
+
+
+    for num in dir(hidden_4):
+        if num[:2] != "__":
+            print(num)
