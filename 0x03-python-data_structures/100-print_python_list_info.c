8bb5c4b619feaeef1aed8fedcf6ba83801e80fbc
dina3538 <dinaaltayeb1234@gmail.com>
2023-12-04 04:03:32 -0800

    a

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -10,6 +10,6 @@ void print_python_list_info(PyObject *p)
 
 	printf("[*] Size of the Python List = %li\n", s);
 	printf("[*] Allocated = %li\n", obj->allocated);
-	for (k = 0; k < size; k++)
+	for (k = 0; k < s; k++)
 		printf("Element %i: %s\n", k, Py_TYPE(obj->ob_item[k])->tp_name);
 }
