93d3168bf82baaf4cde91fbb8dc2bd8cc07422d2
dina3538 <dinaaltayeb1234@gmail.com>
2023-12-06 04:45:26 -0800

    a

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    t = 0
+    k = 0
+    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in reversed(roman_string):
+        k = num
+        t += k if t < k * 5 else -k
+    return t
