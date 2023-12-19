85facf079cd1d69366f81dd024c7ffedfd87adb7
dina3538 <dinaaltayeb1234@gmail.com>
2023-12-19 13:27:56 -0800

    a

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -10,6 +10,6 @@ class Square:
     """Initialize a new Square.
 
     Args:
-    size (int): The size of the new square.
+        size (int): The size of the new square.
     """
     self.__size = size
