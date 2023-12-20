64a31446d88405966847000da897699408fc8169
dina3538 <dinaaltayeb1234@gmail.com>
2023-12-20 05:20:52 -0800

    a

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    k = 0
+    try:
+        k = a / b
+    except ZeroDivisionError:
+        k = None
+    finally:
+        print('Inside result: {}'.format(k))
+        return k
