3ccd286284b4b5469779694c493f51c461194b16
dina3538 <dinaaltayeb1234@gmail.com>
2024-01-16 07:48:49 -0800

    a

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -26,11 +26,11 @@ def text_indentation(text):
 
     while count < len(text):
         print(text[count], end="")
-            if text[count] == "\n" or text[count] in ".?:":
-                if text[count] in ".?:":
-                    print("\n")
-                count = count + 1
-                while count < len(text) and text[count] == " ":
-                    count = count + 1
-                continue
+        if text[count] == "\n" or text[count] in ".?:":
+            if text[count] in ".?:":
+                print("\n")
             count = count + 1
+            while count < len(text) and text[count] == " ":
+                count = count + 1
+            continue
+        count = count + 1
