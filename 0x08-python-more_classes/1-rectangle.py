4b5b7d7424a64f480634d5e2342a68c99cd7b279
dina3538 <dinaaltayeb1234@gmail.com>
2024-01-16 09:58:26 -0800

    a

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,9 +13,9 @@ class Rectangle:
         Raises:
             TypeError: if size is not integer
             ValueError: if size is less than zero
-            """
-            self.width = width
-            self.height = height
+        """
+        self.width = width
+        self.height = height
 
         @property
         def width(self):
