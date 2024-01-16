c9660653f528a5cf4a7d5b57ea89a97b13edef9a
dina3538 <dinaaltayeb1234@gmail.com>
2024-01-16 09:05:25 -0800

    a

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -72,4 +72,8 @@ def matrix_mul(m_a, m_b):
             my_row.append(product)
         matrix2.append(my_row)
 
-    return matrix2
+    return matrix
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/100-matrix_mul.txt")
